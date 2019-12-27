b594f5bc6852edc78408d1ce56a4b42b84984a02
Arindam Das <dasarindam.mails@gmail.com>
2019-12-27 14:13:38 +0530

    Corrected typos, refactored inner lambdas to functions, moved load_daset inner func to an external load_dataset_segment_function

--- esrgan/data/loader.py
+++ esrgan/data/loader.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-from esrgan.utils.helpers import load_config
+from esrgan.util.helpers import load_config
 
-from tf.data.experimental import AUTOTUNE
 
 def get_file_paths_ds(datadir: str, folder: str) -> tf.data.Dataset:
     subdir = ''
@@ -34,17 +33,17 @@ def rotate(x: tf.Tensor, seed: int) -> tf.Tensor:
     """
 
     # Rotate 0, 90, 180, 270 degrees
-    return tf.image.rot90(x, 
-                          tf.random_uniform(
-                              shape=[], 
-                              minval=0, 
-                              maxval=4, 
-                              dtype=tf.int32, 
+    return tf.image.rot90(x,
+                          tf.random.uniform(
+                              shape=[],
+                              minval=0,
+                              maxval=4,
+                              dtype=tf.int32,
                               seed=seed)
                           )
 
 
-def flip(x: tf.Tensor, seed: int) -> tf.Tensor:
+def horizontal_flip(x: tf.Tensor, seed: int) -> tf.Tensor:
     """Flip augmentation
 
     Args:
@@ -53,37 +52,42 @@ def flip(x: tf.Tensor, seed: int) -> tf.Tensor:
     Returns:
         Augmented image
     """
-    x = tf.image.random_flip_left_right(x, 
+    x = tf.image.random_flip_left_right(x,
                                         seed=seed)
     return x
 
 
-def get_augmented_ds_from_paths (paths_ds: tf.data.Dataset, seed: int) -> tf.data.Dataset:
+def get_augmented_ds_from_paths(paths_ds: tf.data.Dataset, seed: int) -> tf.data.Dataset:
     original_ds = paths_ds.map(load_image_from_path,
-                               num_parallel_calls=AUTOTUNE)
+                               num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
     augmented_ds = tf.data.Dataset()
     augmented_ds.concatenate(original_ds)
 
-    rotate_fn = lambda x: rotate(x, seed)
-    flip_fn = lambda x: flip(x, seed)
+    def rotate_fn(img):
+        return rotate(img, seed)
+
+    def flip_fn(img):
+        return horizontal_flip(img, seed)
 
     for map_func in [rotate_fn, flip_fn]:
         augmented_ds.concatenate(
             original_ds.map(
                 map_func=map_func,
-                num_parallel_calls=AUTOTUNE
+                num_parallel_calls=tf.data.experimental.AUTOTUNE
             )
         )
 
     return augmented_ds
 
-def load_datasets(config=load_config()) -> (tf.data.Dataset, tf.data.Dataset):
-    def load_dataset(dataset):
-        lrds = get_file_paths_ds(config['datadir'], f'{dataset}_lr')
-        hrds = get_file_paths_ds(config['datadir'], f'{dataset}_hr')
-        lrds = get_augmented_ds_from_paths(lrds, config['seed'])
-        lrds = get_augmented_ds_from_paths(lrds, config['seed'])
-        return tf.data.Dataset.zip(lrds, hrds)
 
-    return load_dataset('train'), load_dataset('validation')
+def load_dataset_segment(segment: str, config=load_config()) -> tf.data.Dataset:
+    lrds = get_file_paths_ds(config['datadir'], f'{segment}_lr')
+    hrds = get_file_paths_ds(config['datadir'], f'{segment}_hr')
+    lrds = get_augmented_ds_from_paths(lrds, config['seed'])
+    lrds = get_augmented_ds_from_paths(lrds, config['seed'])
+    return tf.data.Dataset.zip(lrds, hrds)
+
+
+def load_datasets(config=load_config()) -> (tf.data.Dataset, tf.data.Dataset):
+    return load_dataset_segment('train', config), load_dataset_segment('validation', config)
